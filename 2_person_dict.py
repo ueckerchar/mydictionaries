48a5d790f3e962b9057c4c774b08aa2e1741a62a
Charlie <Charles_Uecker1@baylor.edu>
2022-09-20 11:43:21 -0500

    More Make-Up Sept13

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,13 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+print(person['children'][1])
+
+print(person['pets']['cat'])
+
+for child in person['children']:
+    print(person)
+
+for p in person['pets']:
+    print(person['pets'][p])
--- 5_datastore.py
+++ 5_datastore.py
@@ -47,3 +47,13 @@ datastore = { "medical":[
 
       ]
 }
+
+outfile = open('retail_space2.csv','w')
+outfile.write('room-number,use,sq-ft,price\n')
+
+
+for x in datastore['medical']:
+  outfile.write(str(x['room-number'])+','+str(x['use'])+','+str(x['sq-ft'])+','+str(x[price]))
+
+
+outfile.close()
